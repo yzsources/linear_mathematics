2ff3c1dfa810aaff576b18d884241e40f5241641
Юрий Захарян < >
2017-05-21 23:51:41 +0300

    Testing git after installation

--- linear_mathematics/Program.cs
+++ linear_mathematics/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace linear_mathematics
+﻿namespace linear_mathematics
 {
     class Program
     {

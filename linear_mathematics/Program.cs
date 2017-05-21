6078260de17c186050642f8402fcc93778698e0e
Юрий Захарян < >
2017-05-21 23:42:05 +0300

    Add project files.

---
+++ linear_mathematics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linear_mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

769d9b2dbda93f8f753f3cac9043bb434deff852
Юрий Захарян < >
2017-05-25 05:11:46 +0300

    Remove all tested objects

--- linear_mathematics/Program.cs
+++ linear_mathematics/Program.cs
@@ -1,5 +1,4 @@
-﻿using linear_mathematics.Test_objects;
-using System;
+﻿using System;
 
 namespace linear_mathematics
 {
@@ -7,10 +6,6 @@ namespace linear_mathematics
     {
         static void Main(string[] args)
         {
-            var c1 = new ComplexNumber(1, 0);
-            var c2 = new ComplexNumber(0, 1);
-            Console.WriteLine(((ComplexNumber)(Abstract.Twice(c1)+c2)).Norm.ToString());
-            Console.ReadKey();
         }
     }
 }
